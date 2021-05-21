657f4c884a335c2c15759defd4f801d75ad217fd
LeandroFilie <leandro.gf03@gmail.com>
2021-05-21 17:45:07 -0300

    Paginas HTML

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(function(){
+
+    // mudar cor da nav
+    $(window).scroll(function(){
+        if ($(this).scrollTop() === 0) {
+            $(".nav-index").css('background-color', 'rgba(0,0,0,0)');
+        } 
+        else{
+            $(".nav-index").css('background-color', 'rgba(0,0,0,0.6)');
+        }
+    });
+
+    // Menu Responsivo
+    $('#js-open-menu').click(function(){
+        $(this).toggleClass('menu-opened');
+
+        $(this).toggleClass('active');
+        $(".menu").toggleClass('active');
+    });
+
+    $(".go-about").click(function(){
+        $(this).removeClass('active');
+        $(".menu").removeClass('active');
+
+        $('.menu-button').removeClass('menu-opened');
+    });
+});
