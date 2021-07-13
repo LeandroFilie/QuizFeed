4729f1f12e2e8af9e9f4d707dfa55ce2e597e7c2
LeandroFilie <leandro.gf03@gmail.com>
2021-07-13 14:44:04 -0300

    Layout home usuario que ja esta na rede

--- home.php
+++ home.php
@@ -49,38 +49,86 @@
                 ';
             }
             else if($_SESSION["permissao"] == 2){
-                echo '
-                    <div class="section-title">
-                        <h1 class="title">Seja bem-vindo, @'.$_SESSION["nome_usuario"].'</h1>
-                     </div>
-                    <section class="section-description-question">
-                        <p>Me fala, você já fez alguma orientação vocacional com algum especialista?</p>
-                    </section>
-                    <section id="tabs">
-                        <div class="tab-links">
-                            <button id="option-1">Sim, já fiz</button>
-                            <button id="option-2">Não, nunca fiz</button>
-                        </div>
+                //USUÁRIO SEM REDE
+                if(){
+                    echo '
+                        <div class="section-title">
+                            <h1 class="title">Seja bem-vindo, @'.$_SESSION["nome_usuario"].'</h1>
+                         </div>
+                        <section class="section-description-question">
+                            <p>Me fala, você já fez alguma orientação vocacional com algum especialista?</p>
+                        </section>
+                        <section id="tabs">
+                            <div class="tab-links">
+                                <button id="option-1">Sim, já fiz</button>
+                                <button id="option-2">Não, nunca fiz</button>
+                            </div>
+    
+                            <div class="tab-content">
+                                <section id="option-1-content">
+                                    <p>Então escolha a sua área de maior afinidade</p>
+                                </section>
+                                <section id="option-2-content">
+                                    <div class="section-cta">
+                                        <div class="cta-psicologos">
+                                            <p>Veja profissionais especializados para você fazer uma orientação vocacional</p>
+                                            <a href="./dados_psicologos.php"><button>Ver mais</button></a>
+                                        </div>
+                                        <div class="cta-testes">
+                                            <p>Veja testes vocacionais online que podem te ajudar também</p>
+                                            <button>Ver mais</button>
+                                        </div>
+                                    </div>
+                                </section>
+                            </div>
+                        </section>
+                    ';
+                }
+                else{ //USUÁRIO COM REDE
+                    echo '
+                        <section class="cards">
 
-                        <div class="tab-content">
-                            <section id="option-1-content">
-                                <p>Então escolha a sua área de maior afinidade</p>
+                            <section class="card-area">
+                                <p>Sua área de maior afinidade é</p>
+                                <h2>Ciências Exatas</h2>
+                                <span>Ainda está em dúvida? Quer ir para outra rede? <a href="" class="link">Clique Aqui</a></span>
+                                <button>Meus Posts</button>
+                                <button>Meus Dados</button>
                             </section>
-                            <section id="option-2-content">
-                                <div class="section-cta">
-                                    <div class="cta-psicologos">
-                                        <p>Veja profissionais especializados para você fazer uma orientação vocacional</p>
-                                        <a href="./dados_psicologos.php"><button>Ver mais</button></a>
+                    
+                            <section class="card-rede">
+                                <h2>Últimos Posts</h2>
+                                <div class="last-posts">
+                        
+                                    <div class="post">
+                                    <div class="data-user">
+                                        <img src="./assets/avatar.svg" alt="avatar">
+                                        <span>NomeUsuario</span>
                                     </div>
-                                    <div class="cta-testes">
-                                        <p>Veja testes vocacionais online que podem te ajudar também</p>
-                                        <button>Ver mais</button>
+                        
+                                    <div class="conteudo">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit sint quasi dolorem eligendi, modi consectetur a! At aut corrupti culpa sint saepe error, eum, porro velit ipsa, voluptas quo placeat.
                                     </div>
+                                    </div>
+                        
+                                    <div class="post">
+                                    <div class="data-user">
+                                        <img src="./assets/avatar.svg" alt="avatar">
+                                        <span>NomeUsuario</span>
+                                    </div>
+                        
+                                    <div class="conteudo">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit sint quasi dolorem eligendi, modi consectetur a! At aut corrupti culpa sint saepe error, eum, porro velit ipsa, voluptas quo placeat.
+                                    </div>
+                                    </div>
+                        
                                 </div>
+                                <button>Ver Mais</button>
                             </section>
-                        </div>
-                    </section>
-                ';
+                            
+                        </section>
+                    ';
+                }
             }
             else if($_SESSION["permissao"] == 3){
                 if($_SESSION["situacao"] == 1){
