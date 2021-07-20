e40985b0502293232f7805fa4a2e4679f02f4aa3
CarolMotta16 <carolinemotta@hotmail.com.br>
2021-07-20 19:17:13 -0300

    carol

---
+++ areas.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <?php include './inc/head.inc' ?>
+    <link rel="stylesheet" href="./style/index.css">
+    <title>Explicação das Áreas | TesteFeed</title>
+</head>
+<body>
+    <header class="header-index">   
+        <div class="nav">
+            <nav class="nav-index">
+                <a href="home.php"><img src="./assets/images/logo.svg" alt="logo" class="logo"></a>
+                <button id="js-open-menu" class="menu-button">
+                    <i class="menu-icon"></i>
+                </button>
+            </nav>   
+        </div> 
+        <div class="card" style="width: 18rem;">
+        </div>
+
+        <div class="row">
+        <div class="col-sm-6">
+            <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Ciências Exatas e da Terra</h5>
+                <p class="card-text">Primeiramente, você precisa ter em mente que a matemática e os números são a base dessa área! Ela pode englobar cursos muito diferentes, mas como característica em comum, são cursos baseados em cálculos físico-matemáticos. Engloba a matemática, física, computação, astromia, química, mecânica e vertentes que esses cursos podem ter.</p>
+                <a href="#" class="btn btn-primary">quero ir pra essa rede!</a>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Ciências Biológicas</h5>
+                <p class="card-text">Essa é a área da biologia. Tudo nela vai se relacionar com a natureza, e vai englobar estudos aprofundados sobre toda vida existente no planeta Terra. Você precisa estar preparado pra aprender tudo nos mínimos detalhes, desde quando tudo surgiu até hoje!</p>
+                <a href="#" class="btn btn-primary">quero ir pra essa rede!</a>
+            </div>
+            </div>
+
+            <div class="col-sm-6">
+            <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Engenharias</h5>
+                <p class="card-text">Na área das engenharias, a ciência é aplicada e utilizada na prática, atuando muito na economia. Atualmente, existem 34 tipos de engenharia pra você decidir em qual se encaixa melhor. Vale lembrar que envolve cálculos e a habilidade que você tem com a criação, já que na prática vão ter muitos processos, inclusive o de criação!</p>
+                <a href="#" class="btn btn-primary">quero ir pra essa rede!</a>
+            </div>
+
+            <div class="col-sm-6">
+            <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Ciências da Saúde</h5>
+                <p class="card-text">Como o prório nome diz, trata de tudo relacionado a saúde, seja atendendo diretamente o público ou atuando na área da pesquisa. O curso vai envolver muita biologia e conhecimento do corpo humano. Vale ressaltar que a saúde não envolve somente a medicina, mas também outros cursos, como a psicologia, fisioterapia, fonoaudiologia, dentre tantos outros.</p>
+                <a href="#" class="btn btn-primary">quero ir pra essa rede!</a>
+            </div>
+            <div class="col-sm-6">
+            <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Ciências Agrárias</h5>
+                <p class="card-text">Bom, essa área se envolve de uma maneira única com a natureza, principalmente em sua aplicação. Possui diversos cursos, que buscam melhorar a questão da preservação do meio ambiente, utilizando a tecnologia e o conhecimento como base e auxílio. Envolve de A até Z: desde agronomia até a zootecnia! Sua área de atuação irá depender muito do lugar em que você estará situado.</p>
+                <a href="#" class="btn btn-primary">quero ir pra essa rede!</a>
+            </div>
+            </div>
+
+            <div class="col-sm-6">
+            <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Ciências Sociais Aplicadas</h5>
+                <p class="card-text">Possui diversos cursos em sua área, mas todos estão conectados com um propósito: entender a sociedade, suas necessidades e a convivência nela, principalmente as consequências dessa convivência. As ciências sociais envolvem desde a administração, audiovisual (cinema, vídeo, rádio, TV e internet), e comunicação, até o próprio curso de ciências sociais. É necessário estudar o indivíduo e suas atitudes, o que elas refletem na sociedade. Para isso, é necessário saber de todas as teorias que são usadas para entender tantas coisas!</p>
+                <a href="#" class="btn btn-primary">quero ir pra essa rede!</a>
+            </div>
+            <div class="col-sm-6">
+            <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Ciências Humanas</h5>
+                <p class="card-text">Nessa área, será estudo o ser humano, mas não como na saúde ou a biologia, que estuda o corpo, mas será estudado seus ideais, suas diferentes histórias e culturas, acontecimentos marcantes. Podemos citar a história, artes, filosofia e todas as suas vertentes, até mesmo a geografia em sua parte social, com o estudo da globalização e das mudanças causadas pelo ser humano até hoje.</p>
+                <a href="#" class="btn btn-primary">quero ir pra essa rede!</a>
+            </div>
+            </div>
+            <div class="col-sm-6">
+            <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Linguística, Letras e Artes</h5>
+                <p class="card-text">Trata da comunicação! Seja ela através de palavras, numa língua, ou seja ela expressada através da arte. Além de poder estudar línguas e estrutura linguística, também pode trazer todas as formas de arte, sejam no cinema, obras, teatro ou músicas!</p>
+                <a href="#" class="btn btn-primary">quero ir pra essa rede!</a>
+            </div>
+            </div>
+
+        </div>
+        </div>
+        
+    </section>
+
+    <?php include './inc/footer.inc' ?>
+
+</body>
+</html>
---
+++ faq.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <?php include './inc/head.inc' ?>
+    <link rel="stylesheet" href="./style/index.css">
+    <title>FAQ | QuizFeed</title>
+</head>
+<body>
+
+<div class="container">
+
+<div class="page-header">
+    <h1>FAQ do QuizFeed <small>dúvidas que você pode ter</small></h1>
+</div>
+
+<div class="container">
+    <br />
+    <br />
+    <br />
+
+    <div class="panel-group" id="accordion">
+        <div class="faqHeader">Perguntas Gerais</div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">pergunta</a>
+                </h4>
+            </div>
+            <div id="collapseOne" class="panel-collapse collapse in">
+                <div class="panel-body">
+                    O trabalho <strong>QuizFeed</strong> resposta
+                </div>
+            </div>
+        </div>
+        
+
+    </div>
+</div>
+
+<?php include './inc/footer.inc' ?>
+
+</body>
+</html>
