f1c3611357d4d87d9f9e2c3e15434ae236bfe824
LeandroFilie <leandro.gf03@gmail.com>
2021-08-03 22:38:16 -0300

    update banco de dados

--- quizfeed.sql
+++ quizfeed.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 22-Jul-2021 às 01:25
+-- Tempo de geração: 04-Ago-2021 às 01:37
 -- Versão do servidor: 5.7.31
 -- versão do PHP: 7.3.21
 
@@ -42,28 +42,27 @@ CREATE TABLE IF NOT EXISTS `area` (
 --
 
 INSERT INTO `area` (`id_area`, `nome`, `descricao`) VALUES
-(1, 'Ciências Exatas e da Terra', ''),
-(2, 'Ciências Biológicas', ''),
-(3, 'Engenharias', ''),
-(4, 'Ciências da Saúde', ''),
-(5, 'Ciências Agrárias', ''),
-(6, 'Ciências Sociais Aplicadas', ''),
-(7, 'Ciências Humanas', ''),
-(8, 'Linguística, Letras e Artes', '');
+(1, 'Ciências Exatas e da Terra', 'Descrição 1'),
+(2, 'Ciências Biológicas', 'Descrição 2'),
+(3, 'Engenharias', 'Descrição 3'),
+(4, 'Ciências da Saúde', 'Descrição 4'),
+(5, 'Ciências Agrárias', 'Descrição 5'),
+(6, 'Ciências Sociais Aplicadas', 'Descrição 6'),
+(7, 'Ciências Humanas', 'Descrição 7'),
+(8, 'Linguística, Letras e Artes', 'Descrição 8');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `area_resultado`
+-- Estrutura da tabela `area_cursos`
 --
 
-DROP TABLE IF EXISTS `area_resultado`;
-CREATE TABLE IF NOT EXISTS `area_resultado` (
-  `cod_resultado` int(11) NOT NULL,
+DROP TABLE IF EXISTS `area_cursos`;
+CREATE TABLE IF NOT EXISTS `area_cursos` (
   `cod_area` int(11) NOT NULL,
-  PRIMARY KEY (`cod_resultado`,`cod_area`),
-  KEY `cod_area` (`cod_area`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `cod_usuario` int(11) NOT NULL,
+  PRIMARY KEY (`cod_area`,`cod_usuario`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -82,6 +81,19 @@ CREATE TABLE IF NOT EXISTS `comentario` (
   KEY `cod_postagem` (`cod_postagem`),
   KEY `comentario_ibfk_1` (`email_usuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cursos`
+--
+
+DROP TABLE IF EXISTS `cursos`;
+CREATE TABLE IF NOT EXISTS `cursos` (
+  `id_curso` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_curso`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -132,7 +144,7 @@ CREATE TABLE IF NOT EXISTS `postagem` (
   PRIMARY KEY (`id_postagem`,`email_usuario`,`cod_rede`),
   KEY `email_usuario` (`email_usuario`),
   KEY `cod_rede` (`cod_rede`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -162,44 +174,6 @@ INSERT INTO `rede` (`id_rede`, `nome`, `cod_area`) VALUES
 (6, 'Ciências Sociais Aplicadas', 6),
 (7, 'Ciências Humanas', 7),
 (8, 'Linguística, Letras e Artes', 8);
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `resultado`
---
-
-DROP TABLE IF EXISTS `resultado`;
-CREATE TABLE IF NOT EXISTS `resultado` (
-  `id_resultado` int(11) NOT NULL,
-  `cod_teste` int(11) NOT NULL,
-  `descricao` varchar(500) NOT NULL,
-  PRIMARY KEY (`id_resultado`,`cod_teste`),
-  KEY `cod_teste` (`cod_teste`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `teste_pronto`
---
-
-DROP TABLE IF EXISTS `teste_pronto`;
-CREATE TABLE IF NOT EXISTS `teste_pronto` (
-  `id_teste` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(50) NOT NULL,
-  `link` varchar(200) NOT NULL,
-  PRIMARY KEY (`id_teste`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `teste_pronto`
---
-
-INSERT INTO `teste_pronto` (`id_teste`, `nome`, `link`) VALUES
-(1, 'Guia da Carreira', 'https://www.guiadacarreira.com.br/teste-vocacional/'),
-(2, 'Quero Bolsa', 'https://querobolsa.com.br/teste-vocacional-gratis'),
-(3, 'Vix', 'https://www.vix.com/pt/comportamento/546867/qual-profissao-mais-combina-com-voce-este-teste-vocacional-te-ajuda-a-descobrir');
 
 -- --------------------------------------------------------
 
@@ -277,33 +251,9 @@ INSERT INTO `usuario_psicologo` (`email_usuario`, `registro`, `cidade`, `uf`, `s
 ('julia@psicologa.com', '11111111111', 'São Paulo', 'SP', 2),
 ('leandro@psicologo.com', '22222222222', 'Ribeirão Preto', 'SP', 2);
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `usuario_testepronto`
---
-
-DROP TABLE IF EXISTS `usuario_testepronto`;
-CREATE TABLE IF NOT EXISTS `usuario_testepronto` (
-  `email_usuario` varchar(100) NOT NULL,
-  `cod_teste` int(11) NOT NULL,
-  `resultado_teste` int(11) NOT NULL,
-  `area` int(11) NOT NULL,
-  `data` date NOT NULL,
-  PRIMARY KEY (`email_usuario`,`cod_teste`),
-  KEY `cod_teste` (`cod_teste`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 --
 -- Restrições para despejos de tabelas
 --
-
---
--- Limitadores para a tabela `area_resultado`
---
-ALTER TABLE `area_resultado`
-  ADD CONSTRAINT `area_resultado_ibfk_1` FOREIGN KEY (`cod_resultado`) REFERENCES `resultado` (`id_resultado`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `area_resultado_ibfk_2` FOREIGN KEY (`cod_area`) REFERENCES `area` (`id_area`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Limitadores para a tabela `comentario`
@@ -340,12 +290,6 @@ ALTER TABLE `rede`
   ADD CONSTRAINT `rede_ibfk_1` FOREIGN KEY (`cod_area`) REFERENCES `area` (`id_area`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Limitadores para a tabela `resultado`
---
-ALTER TABLE `resultado`
-  ADD CONSTRAINT `resultado_ibfk_1` FOREIGN KEY (`cod_teste`) REFERENCES `teste_pronto` (`id_teste`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
 -- Limitadores para a tabela `usuario_comum`
 --
 ALTER TABLE `usuario_comum`
@@ -356,13 +300,6 @@ ALTER TABLE `usuario_comum`
 --
 ALTER TABLE `usuario_psicologo`
   ADD CONSTRAINT `usuario_psicologo_ibfk_1` FOREIGN KEY (`email_usuario`) REFERENCES `usuario` (`email`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Limitadores para a tabela `usuario_testepronto`
---
-ALTER TABLE `usuario_testepronto`
-  ADD CONSTRAINT `usuario_testepronto_ibfk_1` FOREIGN KEY (`email_usuario`) REFERENCES `usuario` (`email`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `usuario_testepronto_ibfk_2` FOREIGN KEY (`cod_teste`) REFERENCES `teste_pronto` (`id_teste`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
