3419ba45e98e226a13cb317b5603ff1e169e371e
CarolMotta16 <carolinemotta@hotmail.com.br>
2021-09-09 12:17:01 -0300

    carol

--- quizfeed.sql
+++ quizfeed.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Tempo de geração: 07-Ago-2021 às 02:38
--- Versão do servidor: 5.7.31
--- versão do PHP: 7.3.21
+-- Host: localhost
+-- Tempo de geração: 09-Set-2021 às 14:41
+-- Versão do servidor: 5.6.51
+-- versão do PHP: 8.0.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `quizfeed`
 --
-CREATE DATABASE IF NOT EXISTS `quizfeed` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `quizfeed`;
 
 -- --------------------------------------------------------
 
@@ -29,40 +27,25 @@ USE `quizfeed`;
 -- Estrutura da tabela `area`
 --
 
-DROP TABLE IF EXISTS `area`;
-CREATE TABLE IF NOT EXISTS `area` (
-  `id_area` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `area` (
+  `id_area` int(11) NOT NULL,
   `nome` varchar(50) NOT NULL,
-  `descricao` varchar(200) NOT NULL,
-  PRIMARY KEY (`id_area`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+  `descricao` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Extraindo dados da tabela `area`
 --
 
 INSERT INTO `area` (`id_area`, `nome`, `descricao`) VALUES
-(1, 'Ciências Exatas e da Terra', 'Descrição 1'),
-(2, 'Ciências Biológicas', 'Descrição 2'),
-(3, 'Engenharias', 'Descrição 3'),
-(4, 'Ciências da Saúde', 'Descrição 4'),
-(5, 'Ciências Agrárias', 'Descrição 5'),
-(6, 'Ciências Sociais Aplicadas', 'Descrição 6'),
-(7, 'Ciências Humanas', 'Descrição 7'),
-(8, 'Linguística, Letras e Artes', 'Descrição 8');
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `area_cursos`
---
-
-DROP TABLE IF EXISTS `area_cursos`;
-CREATE TABLE IF NOT EXISTS `area_cursos` (
-  `cod_area` int(11) NOT NULL,
-  `cod_usuario` int(11) NOT NULL,
-  PRIMARY KEY (`cod_area`,`cod_usuario`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+(1, 'Ciências Exatas e da Terra', 'Primeiramente, você precisa ter em mente que a matemática e os números são a base dessa área! Ela pode englobar cursos muito diferentes, mas como característica em comum, são cursos baseados em cálculos físico-matemáticos. Engloba a matemática, física, computação, astromia, química, mecânica e vertentes que esses cursos podem ter.'),
+(2, 'Ciências Biológicas', 'Essa é a área da biologia. Tudo nela vai se relacionar com a natureza, e vai englobar estudos aprofundados sobre toda vida existente no planeta Terra. Você precisa estar preparado pra aprender tudo nos mínimos detalhes, desde quando tudo surgiu até hoje!'),
+(3, 'Engenharias', 'Na área das engenharias, a ciência é aplicada e utilizada na prática, atuando muito na economia. Atualmente, existem 34 tipos de engenharia pra você decidir em qual se encaixa melhor. Vale lembrar que envolve cálculos e a habilidade que você tem com a criação, já que na prática vão ter muitos processos, inclusive o de criação!'),
+(4, 'Ciências da Saúde', 'Como o próprio nome diz, trata de tudo relacionado a saúde, seja atendendo diretamente o público ou atuando na área da pesquisa. O curso vai envolver muita biologia e conhecimento do corpo humano. Vale ressaltar que a saúde não envolve somente a medicina, mas também outros cursos, como a psicologia, fisioterapia, fonoaudiologia, dentre tantos outros.'),
+(5, 'Ciências Agrárias', 'Bom, essa área se envolve de uma maneira única com a natureza, principalmente em sua aplicação. Possui diversos cursos, que buscam melhorar a questão da preservação do meio ambiente, utilizando a tecnologia e o conhecimento como base e auxílio. Envolve de A até Z: desde agronomia até a zootecnia! Sua área de atuação irá depender muito do lugar em que você estará situado.\n\nBom, essa área se envolve de uma maneira única com a natureza, principalmente em sua aplicação. Possui diversos cursos, que buscam melhorar a questão da preservação do meio ambiente, utilizando a tecnologia e o conhecimento como base e auxílio. Envolve de A até Z: desde agronomia até a zootecnia! Sua área de atuação irá depender muito do lugar em que você estará situado.\n\n'),
+(6, 'Ciências Sociais Aplicadas', 'Possui diversos cursos em sua área, mas todos estão conectados com um propósito: entender a sociedade, suas necessidades e a convivência nela, principalmente as consequências dessa convivência. As ciências sociais envolvem desde a administração, audiovisual (cinema, vídeo, rádio, TV e internet), e comunicação, até o próprio curso de ciências sociais. É necessário estudar o indivíduo e suas atitudes, o que elas refletem na sociedade. Para isso, é necessário saber de todas as teorias que são usadas para entender tantas coisas!'),
+(7, 'Ciências Humanas', 'Nessa área, será estudo o ser humano, mas não como na saúde ou a biologia, que estuda o corpo, mas será estudado seus ideais, suas diferentes histórias e culturas, acontecimentos marcantes. Podemos citar a história, artes, filosofia e todas as suas vertentes, até mesmo a geografia em sua parte social, com o estudo da globalização e das mudanças causadas pelo ser humano até hoje.'),
+(8, 'Linguística, Letras e Artes', 'Trata da comunicação! Seja ela através de palavras, numa língua, ou seja ela expressada através da arte. Além de poder estudar línguas e estrutura linguística, também pode trazer todas as formas de arte, sejam no cinema, obras, teatro ou músicas!');
 
 -- --------------------------------------------------------
 
@@ -70,30 +53,34 @@ CREATE TABLE IF NOT EXISTS `area_cursos` (
 -- Estrutura da tabela `comentario`
 --
 
-DROP TABLE IF EXISTS `comentario`;
-CREATE TABLE IF NOT EXISTS `comentario` (
+CREATE TABLE `comentario` (
   `email_usuario` varchar(100) NOT NULL,
   `cod_postagem` int(11) NOT NULL,
   `conteudo` varchar(500) NOT NULL,
   `data` date NOT NULL,
-  `hora` time NOT NULL,
-  PRIMARY KEY (`data`,`hora`),
-  KEY `cod_postagem` (`cod_postagem`),
-  KEY `comentario_ibfk_1` (`email_usuario`)
+  `hora` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `cursos`
+-- Estrutura da tabela `curso`
 --
 
-DROP TABLE IF EXISTS `cursos`;
-CREATE TABLE IF NOT EXISTS `cursos` (
-  `id_curso` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `curso` (
+  `id_curso` int(11) NOT NULL,
   `nome` varchar(100) NOT NULL,
-  PRIMARY KEY (`id_curso`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `cod_area` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `curso`
+--
+
+INSERT INTO `curso` (`id_curso`, `nome`, `cod_area`) VALUES
+(1, 'Matemática', 1),
+(3, 'Probabilidade e Estatística', 1),
+(4, 'Ciência da Computação', 1);
 
 -- --------------------------------------------------------
 
@@ -101,14 +88,11 @@ CREATE TABLE IF NOT EXISTS `cursos` (
 -- Estrutura da tabela `curtida`
 --
 
-DROP TABLE IF EXISTS `curtida`;
-CREATE TABLE IF NOT EXISTS `curtida` (
+CREATE TABLE `curtida` (
   `email_usuario` varchar(100) NOT NULL,
   `cod_postagem` int(11) NOT NULL,
   `data` date NOT NULL,
-  `hora` time NOT NULL,
-  PRIMARY KEY (`email_usuario`,`cod_postagem`),
-  KEY `cod_postagem` (`cod_postagem`)
+  `hora` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -117,14 +101,11 @@ CREATE TABLE IF NOT EXISTS `curtida` (
 -- Estrutura da tabela `inscricao`
 --
 
-DROP TABLE IF EXISTS `inscricao`;
-CREATE TABLE IF NOT EXISTS `inscricao` (
+CREATE TABLE `inscricao` (
   `email_usuario` varchar(100) NOT NULL,
   `cod_rede` int(11) NOT NULL,
   `data` date NOT NULL,
-  `hora` time NOT NULL,
-  PRIMARY KEY (`email_usuario`,`cod_rede`),
-  KEY `cod_rede` (`cod_rede`)
+  `hora` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -133,18 +114,14 @@ CREATE TABLE IF NOT EXISTS `inscricao` (
 -- Estrutura da tabela `postagem`
 --
 
-DROP TABLE IF EXISTS `postagem`;
-CREATE TABLE IF NOT EXISTS `postagem` (
-  `id_postagem` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `postagem` (
+  `id_postagem` int(11) NOT NULL,
   `data` date NOT NULL,
   `hora` time NOT NULL,
   `conteudo` varchar(500) NOT NULL,
   `situacao` int(11) NOT NULL,
   `email_usuario` varchar(100) NOT NULL,
-  `cod_rede` int(11) NOT NULL,
-  PRIMARY KEY (`id_postagem`,`email_usuario`,`cod_rede`),
-  KEY `email_usuario` (`email_usuario`),
-  KEY `cod_rede` (`cod_rede`)
+  `cod_rede` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -153,14 +130,11 @@ CREATE TABLE IF NOT EXISTS `postagem` (
 -- Estrutura da tabela `rede`
 --
 
-DROP TABLE IF EXISTS `rede`;
-CREATE TABLE IF NOT EXISTS `rede` (
-  `id_rede` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `rede` (
+  `id_rede` int(11) NOT NULL,
   `nome` varchar(50) NOT NULL,
-  `cod_area` int(11) NOT NULL,
-  PRIMARY KEY (`id_rede`),
-  KEY `cod_area` (`cod_area`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+  `cod_area` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Extraindo dados da tabela `rede`
@@ -182,13 +156,11 @@ INSERT INTO `rede` (`id_rede`, `nome`, `cod_area`) VALUES
 -- Estrutura da tabela `usuario`
 --
 
-DROP TABLE IF EXISTS `usuario`;
-CREATE TABLE IF NOT EXISTS `usuario` (
+CREATE TABLE `usuario` (
   `email` varchar(100) NOT NULL,
   `nome` varchar(100) NOT NULL,
   `senha` varchar(50) NOT NULL,
-  `permissao` int(1) NOT NULL,
-  PRIMARY KEY (`email`)
+  `permissao` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -210,11 +182,9 @@ INSERT INTO `usuario` (`email`, `nome`, `senha`, `permissao`) VALUES
 -- Estrutura da tabela `usuario_comum`
 --
 
-DROP TABLE IF EXISTS `usuario_comum`;
-CREATE TABLE IF NOT EXISTS `usuario_comum` (
+CREATE TABLE `usuario_comum` (
   `email_usuario` varchar(100) NOT NULL,
-  `nome_usuario` varchar(100) NOT NULL,
-  PRIMARY KEY (`email_usuario`)
+  `nome_usuario` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -233,15 +203,13 @@ INSERT INTO `usuario_comum` (`email_usuario`, `nome_usuario`) VALUES
 -- Estrutura da tabela `usuario_psicologo`
 --
 
-DROP TABLE IF EXISTS `usuario_psicologo`;
-CREATE TABLE IF NOT EXISTS `usuario_psicologo` (
+CREATE TABLE `usuario_psicologo` (
   `email_usuario` varchar(100) NOT NULL,
   `telefone` varchar(30) NOT NULL,
   `registro` varchar(11) NOT NULL,
   `cidade` varchar(50) NOT NULL,
   `uf` varchar(2) NOT NULL,
-  `situacao` int(1) NOT NULL,
-  PRIMARY KEY (`email_usuario`)
+  `situacao` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -254,6 +222,106 @@ INSERT INTO `usuario_psicologo` (`email_usuario`, `telefone`, `registro`, `cidad
 ('leandro@psicologo.com', '(33) 3333-3333', '22222222222', 'Ribeirão Preto', 'SP', 2);
 
 --
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `area`
+--
+ALTER TABLE `area`
+  ADD PRIMARY KEY (`id_area`);
+
+--
+-- Índices para tabela `comentario`
+--
+ALTER TABLE `comentario`
+  ADD PRIMARY KEY (`data`,`hora`),
+  ADD KEY `cod_postagem` (`cod_postagem`),
+  ADD KEY `comentario_ibfk_1` (`email_usuario`);
+
+--
+-- Índices para tabela `curso`
+--
+ALTER TABLE `curso`
+  ADD PRIMARY KEY (`id_curso`),
+  ADD KEY `cod_area` (`cod_area`);
+
+--
+-- Índices para tabela `curtida`
+--
+ALTER TABLE `curtida`
+  ADD PRIMARY KEY (`email_usuario`,`cod_postagem`),
+  ADD KEY `cod_postagem` (`cod_postagem`);
+
+--
+-- Índices para tabela `inscricao`
+--
+ALTER TABLE `inscricao`
+  ADD PRIMARY KEY (`email_usuario`,`cod_rede`),
+  ADD KEY `cod_rede` (`cod_rede`);
+
+--
+-- Índices para tabela `postagem`
+--
+ALTER TABLE `postagem`
+  ADD PRIMARY KEY (`id_postagem`,`email_usuario`,`cod_rede`),
+  ADD KEY `email_usuario` (`email_usuario`),
+  ADD KEY `cod_rede` (`cod_rede`);
+
+--
+-- Índices para tabela `rede`
+--
+ALTER TABLE `rede`
+  ADD PRIMARY KEY (`id_rede`),
+  ADD KEY `cod_area` (`cod_area`);
+
+--
+-- Índices para tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Índices para tabela `usuario_comum`
+--
+ALTER TABLE `usuario_comum`
+  ADD PRIMARY KEY (`email_usuario`);
+
+--
+-- Índices para tabela `usuario_psicologo`
+--
+ALTER TABLE `usuario_psicologo`
+  ADD PRIMARY KEY (`email_usuario`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `area`
+--
+ALTER TABLE `area`
+  MODIFY `id_area` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de tabela `curso`
+--
+ALTER TABLE `curso`
+  MODIFY `id_curso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `postagem`
+--
+ALTER TABLE `postagem`
+  MODIFY `id_postagem` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `rede`
+--
+ALTER TABLE `rede`
+  MODIFY `id_rede` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- Restrições para despejos de tabelas
 --
 
@@ -263,6 +331,12 @@ INSERT INTO `usuario_psicologo` (`email_usuario`, `telefone`, `registro`, `cidad
 ALTER TABLE `comentario`
   ADD CONSTRAINT `comentario_ibfk_1` FOREIGN KEY (`email_usuario`) REFERENCES `usuario` (`email`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `comentario_ibfk_2` FOREIGN KEY (`cod_postagem`) REFERENCES `postagem` (`id_postagem`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limitadores para a tabela `curso`
+--
+ALTER TABLE `curso`
+  ADD CONSTRAINT `curso_ibfk_1` FOREIGN KEY (`cod_area`) REFERENCES `area` (`id_area`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Limitadores para a tabela `curtida`
